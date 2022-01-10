5445a07b398d21608fbe35be99ff7ff58646f192
Victor Ibañez Emborujo <28122002victor@gmail.com>
2022-01-10 13:25:54 +0100

    cambios

---
+++ TEMA_6/Practica_2 - Ejercicio_1/src/Modelo/Estudio.java
@@ -0,0 +1,11 @@
+package Modelo;
+
+public class Estudio {
+    private String nombre;
+    private String ciudad;
+    private String direccion;
+    private String direccionWeb;
+    private int fundacion;
+    private String pais;
+    private int telefono;
+}
---
+++ TEMA_6/Practica_2 - Ejercicio_1/src/Modelo/Pelicula.java
@@ -0,0 +1,11 @@
+package Modelo;
+
+import javax.swing.*;
+
+public class Pelicula {
+    private String titulo;
+    private int año;
+    private int duracion;
+    private String tipo;
+}
+
