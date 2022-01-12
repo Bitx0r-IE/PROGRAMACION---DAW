dd76e3faea6ccf2db1a2daf275adbf141b924361
Victor Ibañez Emborujo <28122002victor@gmail.com>
2022-01-12 11:20:18 +0100

    si

--- TEMA_6/Practica_2 - Ejercicio_1/src/com/company/Main.java
+++ TEMA_6/Practica_2 - Ejercicio_1/src/com/company/Main.java
@@ -1,10 +1,12 @@
 package com.company;
+import java.util.ArrayList;
+import Modelo.*;
 
 public class Main {
 
     public static void main(String[] args) {
 	    // write your code here
-
-
+        Pelicula pelicula[] = new Pelicula[0];
+        pelicula =
     }
 }
