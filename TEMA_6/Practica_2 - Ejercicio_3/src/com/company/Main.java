b1c7b219b2a7fcb1f504959aa7d0bae672f857e8
Victor Ibañez Emborujo <28122002victor@gmail.com>
2022-01-14 11:00:25 +0100

    new objects

--- TEMA_6/Practica_2 - Ejercicio_3/src/com/company/Main.java
+++ TEMA_6/Practica_2 - Ejercicio_3/src/com/company/Main.java
@@ -1,13 +1,14 @@
 package com.company;
 
-import Modelo.Persona;
-
+import Modelo.*;
 import javax.swing.JOptionPane;
 import java.util.ArrayList;
 
 public class Main {
 
-    private static ArrayList<Persona> listaPersonas;
+    private static ArrayList<Empleado> listaEmpleados;
+    private static ArrayList<Cliente> listaClientes;
+    private static ArrayList<Directivo> listaDirectivos;
 
     public static void main(String[] args) {
         // try catch generico:
@@ -19,7 +20,28 @@ public class Main {
         }
     }
     public static void crearPersona(){
-        listaPersonas = new ArrayList();
-        listaPersonas.add(new Persona("Pepe", 19));
+        //Empleados:
+        listaEmpleados = new ArrayList();
+        //Empleado 1:
+        listaEmpleados.add(new Empleado("Pepe", 19, 12000));
+        //Empleado 2:
+        listaEmpleados.add(new Empleado("Juan", 27, 15000));
+        //Empleado 3:
+        listaEmpleados.add(new Empleado("Manolo", 39, 16000));
+        //Empleado 4:
+        listaEmpleados.add(new Empleado("Leire", 30, 16000));
+
+        //Clientes:
+        listaClientes = new ArrayList();
+        //Cliente 1:
+        listaClientes.add(new Cliente("Pepa", 52, 111111111));
+        //Cliente 2:
+        listaClientes.add(new Cliente("Juana", 80, 222222222));
+        //Cliente 3:
+        listaClientes.add(new Cliente("Eugenio", 60, 333333333));
+
+        //Directivos:
+        listaDirectivos = new ArrayList();
+        //Directivo 1:
     }
 }
