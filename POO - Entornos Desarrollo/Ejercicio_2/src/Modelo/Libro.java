e8fd782fecc15cd875ea410f0db35a40cc895d08
Victor Ibañez Emborujo <28122002victor@gmail.com>
2022-01-14 13:42:25 +0100

    Completar ejercicio 2 ED POO

--- POO - Entornos Desarrollo/Ejercicio_2/src/Modelo/Libro.java
+++ POO - Entornos Desarrollo/Ejercicio_2/src/Modelo/Libro.java
@@ -4,5 +4,53 @@ public class Libro {
     private int isbn;
     private String titulo;
     private String autor;
-    
+    private int numPaginas;
+
+    //Constructor:
+
+    public Libro(int isbn, String titulo, String autor, int numPaginas) {
+        this.isbn = isbn;
+        this.titulo = titulo;
+        this.autor = autor;
+        this.numPaginas = numPaginas;
+    }
+
+    //set y get:
+    public int getIsbn() {
+        return isbn;
+    }
+
+    public void setIsbn(int isbn) {
+        this.isbn = isbn;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getAutor() {
+        return autor;
+    }
+
+    public void setAutor(String autor) {
+        this.autor = autor;
+    }
+
+    public int getNumPaginas() {
+        return numPaginas;
+    }
+
+    public void setNumPaginas(int numPaginas) {
+        this.numPaginas = numPaginas;
+    }
+
+    @Override
+    public String toString() {
+        return "El libro con el ISBN " + isbn + " creado por el autor " + autor + " tiene " + numPaginas + " páginas";
+    }
+
 }
--- POO - Entornos Desarrollo/Ejercicio_2/src/com/company/Main.java
+++ POO - Entornos Desarrollo/Ejercicio_2/src/com/company/Main.java
@@ -1,8 +1,24 @@
 package com.company;
 
+import Modelo.Libro;
+import javax.swing.*;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+	    // Crear libros:
+        Libro libro1 = new Libro(1111, "El ritmo de la guerra", "Brandon Sanderson", 1376);
+        Libro libro2 = new Libro(2222, "El ojo del mundo", "Robert Jordan", 800);
+
+        //to String:
+        JOptionPane.showMessageDialog(null, libro1.toString() + "\n" + libro2.toString());
+
+        //Comparar num paginas de los libros:
+        if (libro1.getNumPaginas() > libro2.getNumPaginas()){
+            JOptionPane.showMessageDialog(null, libro1.getTitulo() + " tiene más páginas que " + libro2.getTitulo());
+        }
+        else{
+            JOptionPane.showMessageDialog(null, libro2.getTitulo() + " tiene más páginas que " + libro1.getTitulo());
+        }
     }
 }
