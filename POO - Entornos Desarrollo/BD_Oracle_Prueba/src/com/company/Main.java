70ba88b9db81f34d82793d7e74cbc34a862d0ade
Victor Ibañez Emborujo <28122002victor@gmail.com>
2022-03-31 12:01:18 +0200

    31/03/2022

--- POO - Entornos Desarrollo/BD_Oracle_Prueba/src/com/company/Main.java
+++ POO - Entornos Desarrollo/BD_Oracle_Prueba/src/com/company/Main.java
@@ -1,8 +1,51 @@
 package com.company;
 
+import oracle.jdbc.OracleTypes;
+
+import java.sql.CallableStatement;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.util.Calendar;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+	    try {
+            Class.forName("oracle.jdbc.OracleDriver");
+
+            String url = "jdbc:oracle:thin:@172.20.225.114:1521:orcl";
+            String user = "daw25";
+            String passwd = "daw25";
+            Connection con = DriverManager.getConnection(url, user, passwd);
+            /*
+            CallableStatement c = con.prepareCall("{call datos_emp25_scott(?)}");
+
+            //Parámetro de salida
+            c.registerOutParameter(1, oracle.jdbc.OracleTypes.CURSOR);
+            c.execute();
+            ResultSet rs = (ResultSet)c.getObject(1);
+            while (rs.next()){
+                System.out.println(rs.getString(2));
+            }
+            rs.close();
+            c.close();
+            */
+            CallableStatement c = con.prepareCall("{call datos_emp25_scott_v2(?,?)}");
+            c.setInt(1, 1800);
+            c.registerOutParameter(2, oracle.jdbc.OracleTypes.CURSOR);
+
+            c.execute();
+            ResultSet rs = (ResultSet)c.getObject(2);
+            while (rs.next()){
+                System.out.println(rs.getString(2));
+            }
+            rs.close();
+            c.close();
+            con.close();
+        }
+        catch (Exception e){
+            System.out.println(e.getClass());
+        }
     }
 }
